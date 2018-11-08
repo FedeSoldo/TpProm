fc2f7be46c254203b55617420c8db7b9e733aa1f
Rodrigo Etchegaray <rorroeche@gmail.com>
2018-11-08 17:28:45 -0300

    Habemus parte 0 andando.

--- kern/trap.c
+++ kern/trap.c
@@ -225,6 +225,24 @@ trap_dispatch(struct Trapframe *tf)
 	// Handle processor exceptions.
 	// LAB 3: Your code here.
 
+	if (tf->tf_trapno == T_PGFLT) {
+		page_fault_handler(tf);
+		return;
+	}
+ 	if (tf->tf_trapno == T_BRKPT) {
+		monitor(tf);
+		return;
+	}
+ 	if (tf->tf_trapno == T_SYSCALL) {
+		tf->tf_regs.reg_eax = syscall(tf->tf_regs.reg_eax,
+		                              tf->tf_regs.reg_edx,
+		                              tf->tf_regs.reg_ecx,
+		                              tf->tf_regs.reg_ebx,
+		                              tf->tf_regs.reg_edi,
+		                              tf->tf_regs.reg_esi);
+																	return;
+	}
+
 	// Handle spurious interrupts
 	// The hardware sometimes raises these because of noise on the
 	// IRQ line or other reasons. We don't care.
