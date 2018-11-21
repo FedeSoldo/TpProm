da5a7ad8725be2dd21d18526efa355b99d79d88d
Rodrigo Etchegaray <rorroeche@gmail.com>
2018-11-21 15:38:02 -0300

    Modifico lo que recomienda Dato.

--- kern/env.c
+++ kern/env.c
@@ -541,8 +541,8 @@ env_run(struct Env *e)
 
 	// LAB 3: Your code here.
 	// Sigo los pasos recomendados arriba.
-	if (curenv && e->env_status == ENV_RUNNING)
-		e->env_status = ENV_RUNNABLE;
+	if (curenv && curenv->env_status == ENV_RUNNING)
+	curenv->env_status = ENV_RUNNABLE;
 	curenv = e;
 	e->env_status = ENV_RUNNING;
 	e->env_runs++;
