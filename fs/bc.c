323b91c4def24137a59e60e1eec9ea25e1c3f9f5
Rodrigo Etchegaray <rorroeche@gmail.com>
2018-12-02 13:18:54 -0300

    Parte 1 completa.

--- fs/bc.c
+++ fs/bc.c
@@ -83,12 +83,27 @@ void
 flush_block(void *addr)
 {
 	uint32_t blockno = ((uint32_t) addr - DISKMAP) / BLKSIZE;
+	int r;
 
 	if (addr < (void *) DISKMAP || addr >= (void *) (DISKMAP + DISKSIZE))
 		panic("flush_block of bad va %08x", addr);
 
 	// LAB 5: Your code here.
-	panic("flush_block not implemented");
+
+	//Redondeamos la direccion.
+	addr = ROUNDDOWN(addr, PGSIZE);
+	//Chequeamos que este mapeada y el dirty bit en 1.
+	if (va_is_mapped(addr) && va_is_dirty(addr))
+	{
+		//Escribimos en el bloque correspondiente.
+		ide_write(BLKSECTS * blockno, addr, BLKSECTS);
+
+		//Limpiamos el dirty bit.
+		if ((r = sys_page_map(0, addr, 0, addr, uvpt[PGNUM(addr)] & PTE_SYSCALL)) <
+				0)
+			panic("in bc_pgfault, sys_page_map: %e", r);
+		//panic("flush_block not implemented");
+		}
 }
 
 // Test that the block cache works, by smashing the superblock and
