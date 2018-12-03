656cb77365d97fc8d71b30c3752321662dac1a0e
Rodrigo Etchegaray <rorroeche@gmail.com>
2018-12-03 14:35:23 -0300

    sys_env_set_trapframe hecho.

--- kern/syscall.c
+++ kern/syscall.c
@@ -142,7 +142,16 @@ sys_env_set_trapframe(envid_t envid, struct Trapframe *tf)
 	// LAB 5: Your code here.
 	// Remember to check whether the user has supplied us with a good
 	// address!
-	panic("sys_env_set_trapframe not implemented");
+
+	struct Env *e;
+	int ret = envid2env(envid, &e, 1);
+	if (ret < 0) return -E_BAD_ENV;
+	user_mem_assert(e, tf, sizeof(struct Trapframe), PTE_U); //Chequeamos que podamos acceder a ese rango de memoria con este env.
+	e->env_tf = *tf;
+	e->env_tf.tf_eflags |= FL_IF; //Habilitamos interrupts.
+	e->env_tf.tf_cs = GD_UT | 3; 
+	return 0;
+	//panic("sys_env_set_trapframe not implemented");
 }
 
 // Set the page fault upcall for 'envid' by modifying the corresponding struct
@@ -466,6 +475,9 @@ syscall(uint32_t syscallno, uint32_t a1, uint32_t a2, uint32_t a3, uint32_t a4, 
 
 	case SYS_env_set_pgfault_upcall:
 		return sys_env_set_pgfault_upcall(a1, (void*)a2);
+
+	case SYS_env_set_trapframe:
+		return sys_env_set_trapframe(a1, (void *)a2);
 
 	default:
 		return -E_INVAL;
