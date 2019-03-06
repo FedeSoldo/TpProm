03e996d0fbd2a7f419164625a9511669cc5291d9
Federico <federicosoldo@hotmail.com>
2019-03-06 14:34:04 -0300

    Ejercicio 4

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,10 +1,25 @@
 #include <kern/e1000.h>
+#include <inc/string.h>
 
 // LAB 6: Your driver code here
 
 int e1000_attach(struct pci_func *pcif)
 {
   pci_func_enable(pcif);
+  e1000_vaddr_map = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
+  cprintf("\nTengo: 0x%x\nEpero: 0x80080783\n\n", getreg(E1000_STATUS));
 
   return 0;
+}
+
+static uint32_t
+getreg(uint32_t offset)
+{
+  return *(volatile uint32_t *) (e1000_vaddr_map + offset);
+}
+
+static void
+setreg(uint32_t offset, uint32_t value)
+{
+  *(volatile uint32_t *) (e1000_vaddr_map + offset) = value;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -2,7 +2,16 @@
 #define JOS_KERN_E1000_H
 
 #include <kern/pci.h>
+#include <kern/pmap.h>
+
+#define E1000_STATUS 0x00008
 
 int e1000_attach(struct pci_func *pcif);    //Funcion para pci.c
+
+static uint8_t* e1000_vaddr_map; //Variable con direccion del mapeo
+
+static uint32_t getreg(uint32_t offset);
+
+static void setreg(uint32_t offset, uint32_t value);
 
 #endif  // JOS_KERN_E1000_H
