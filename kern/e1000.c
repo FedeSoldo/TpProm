235d05566da8db72977e79b25d14910d25446330
Federico <federicosoldo@hotmail.com>
2019-03-06 14:36:58 -0300

    Ejercicio 5

--- kern/e1000.c
+++ kern/e1000.c
@@ -3,11 +3,15 @@
 
 // LAB 6: Your driver code here
 
+static struct tx_desc tdescs[CANT_DESCS];
+
 int e1000_attach(struct pci_func *pcif)
 {
   pci_func_enable(pcif);
   e1000_vaddr_map = mmio_map_region(pcif->reg_base[0], pcif->reg_size[0]);
-  cprintf("\nTengo: 0x%x\nEpero: 0x80080783\n\n", getreg(E1000_STATUS));
+  //cprintf("\nTengo: 0x%x\nEpero: 0x80080783\n\n", getreg(E1000_STATUS));
+
+  e1000_transmit_init();
 
   return 0;
 }
@@ -22,4 +26,20 @@ static void
 setreg(uint32_t offset, uint32_t value)
 {
   *(volatile uint32_t *) (e1000_vaddr_map + offset) = value;
+}
+
+void e1000_transmit_init()
+{
+  setreg(E1000_TDBAL, PADDR(tdescs));
+  setreg(E1000_TDBAH, 0);
+  setreg(E1000_TDLEN, sizeof(tdescs));
+  setreg(E1000_TDH, 0);
+  setreg(E1000_TDT, 0);
+
+  uint32_t tctl = (E1000_TCTL_EN | E1000_TCTL_PSP | 0x00000010 | 0x00000040);   // Ultimos 2 valores son CT Y COLD
+  setreg(E1000_TCTL, tctl);
+
+  uint32_t tipg = (10 | 4 | 6);  //  (IPGT | IPGR1 | IPGR2) valores en el manual 13.4.34
+  setreg(E1000_TIPG, tipg);
+
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -6,6 +6,26 @@
 
 #define E1000_STATUS 0x00008
 
+//Transmit
+#define E1000_TDBAL    0x03800  /* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAH    0x03804  /* TX Descriptor Base Address High - RW */
+#define E1000_TDLEN    0x03808  /* TX Descriptor Length - RW */
+#define E1000_TDH      0x03810  /* TX Descriptor Head - RW */
+#define E1000_TDT      0x03818  /* TX Descripotr Tail - RW */
+#define E1000_TCTL     0x00400  /* TX Control - RW */
+#define E1000_TIPG     0x00410  /* TX Inter-packet gap -RW */
+
+#define E1000_TCTL_EN     0x00000002    /* enable tx */
+#define E1000_TCTL_PSP    0x00000008    /* pad short packets */
+
+#define E1000_TXD_STAT_DD    0x00000001 /* Descriptor Done */
+#define E1000_TXD_CMD_RS     0x08000000 >> 24 /* Report Status */
+#define E1000_TXD_CMD_EOP    0x01000000 >> 24/* End of Packet */
+
+
+#define CANT_DESCS 32
+
+
 int e1000_attach(struct pci_func *pcif);    //Funcion para pci.c
 
 static uint8_t* e1000_vaddr_map; //Variable con direccion del mapeo
@@ -13,5 +33,18 @@ static uint8_t* e1000_vaddr_map; //Variable con direccion del mapeo
 static uint32_t getreg(uint32_t offset);
 
 static void setreg(uint32_t offset, uint32_t value);
+
+void e1000_transmit_init();
+
+// Transmit Descriptor
+struct tx_desc {
+	uint64_t addr;
+	uint16_t length;
+	uint8_t cso;
+	uint8_t cmd;
+	uint8_t status;
+	uint8_t css;
+	uint16_t special;
+} __attribute__ ((packed));
 
 #endif  // JOS_KERN_E1000_H
